434f9d8d7acc727676afbb45f817e229f5bdb2cd
Parth Tank <parthtank301@gmail.com>
2025-03-15 13:36:26 +0530

    FEAT: implemented useOptimistic hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import NewHook1 from "./components/NewHook1";
 import NewHook2 from "./components/NewHook2";
 import NewHook3 from "./components/NewHook3";
+import NewHook4 from "./components/NewHook4";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/use-action-state" element={<NewHook1 />} />
         <Route path="/use-form-status" element={<NewHook2 />} />
         <Route path="/use-hook" element={<NewHook3 />} />
+        <Route path="/use-optimistic" element={<NewHook4 />} />
       </Routes>
     </>
   );
---
+++ src/components/NewHook4.tsx
@@ -0,0 +1,45 @@
+import { useOptimistic, useState } from "react";
+
+const TODOS = [
+  { id: 1, title: "Buy Coffee", pending: false },
+  { id: 2, title: "Clean Room", pending: false },
+  { id: 3, title: "Wash Clothes", pending: false },
+];
+type TODO = (typeof TODOS)[number];
+
+export default function NewHook4() {
+  const [todos, setTodos] = useState(TODOS);
+  const [optimisticTodos, setOptimisticTodos] = useOptimistic(
+    todos,
+    (state: TODO[], newTodo: TODO) => [...state, newTodo]
+  );
+
+  async function addTodo(formData: FormData) {
+    const title = formData.get("title") as string;
+    const newTodo = {
+      id: Date.now(),
+      title,
+      pending: true,
+    };
+    setOptimisticTodos(newTodo);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    setTodos((prev) => [...prev, { ...newTodo, pending: false }]);
+  }
+
+  return (
+    <div>
+      <form action={addTodo}>
+        <label htmlFor="title">Title: </label>
+        <input type="text" name="title" />
+        <button type="submit">Add</button>
+      </form>
+      <ol>
+        {optimisticTodos.map((item) => (
+          <li key={item.id}>
+            {item.title} {item.pending && <small>(Sending...)</small>}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
